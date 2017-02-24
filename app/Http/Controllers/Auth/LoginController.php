897b001bcf3b6ecf4fe6e16b2258a711be35c675
Rory Hellyer <rory@webmodularity.com>
2017-02-24 00:21:02 -0800

    dev

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use WebModularity\LaravelUser\Http\Controllers\Auth\AuthenticatesSocialUsers;
+use WebModularity\LaravelUser\Http\Controllers\Auth\AuthenticatesAndRegistersSocialUsers;
 
 class LoginController extends Controller
 {
@@ -19,7 +19,7 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers, AuthenticatesSocialUsers;
+    use AuthenticatesUsers, AuthenticatesAndRegistersSocialUsers;
 
     /**
      * Where to redirect users after login.
